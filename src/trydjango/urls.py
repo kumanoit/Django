7da67e187663e2420ce5e89a55f685f33c2f30ef
Amit Kumar <akumar@indeed.com>
2019-02-27 10:59:59 +0530

    3. Views

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from products.views import product_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('product/', product_view, name="product_home")
 ]
