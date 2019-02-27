01bc2f4aadc167987a1e1626c042dfd07a68ac8a
Amit Kumar <akumar@indeed.com>
2019-02-27 11:36:20 +0530

    3.3 Rendering context into templates

--- src/products/views.py
+++ src/products/views.py
@@ -6,4 +6,9 @@ def product_view(request, *args, **kwargs):
     return HttpResponse("<h1>Product View</h1>")
 
 def product_new_view(request, *args, **kwargs):
-    return render(request, "home.html", {})
+    context = {
+        "name": "Kumanoit",
+        "title": "Software Engineer",
+        "vehicles": ['Royal Enfield', "Lamborghini", "Audi"]
+    }
+    return render(request, "home.html", context)
