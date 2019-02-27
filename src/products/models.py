45d102af5c5d6eb31009e5250a2f9f2bd7afcf69
Amit Kumar <akumar@indeed.com>
2019-02-27 14:40:12 +0530

    Updating models

--- src/products/models.py
+++ src/products/models.py
@@ -1,8 +1,13 @@
 from django.db import models
-
 # Create your models here.
 
 class Product(models.Model):
     title = models.TextField()
     description = models.TextField()
-    price = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    summary = models.TextField(default="summay not given")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.title)
